793fa6797dc412e89f9a329586940811791e9d06
Paul-Verdure <paul.verdure@gmail.com>
2022-02-02 16:43:53 +0100

    prisma implemented

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+
+const plantsRouter = require("./routers/plantsRouter");
+const usersRouter = require("./routers/usersRouter");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/plants", plantsRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
